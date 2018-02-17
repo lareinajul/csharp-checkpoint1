82972506482f4785865707f3ef5a4fd84bcf96b2
Julie Adams <lareinajul@htmail>
2018-02-16 21:10:12 -0600

    checkpoint 1 first commit

---
+++ DivisibleBy3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    
+public class Exercise39
+{
+    public static void Main()
+    {
+        int i, sum = 0;
+
+        Console.Write("\n\n");
+        Console.Write("Find the number and sum of all integer between 0 and 100, divisible by 3:\n");
+        Console.Write("-----------------------------------------------------------------------------");
+        Console.Write("\n\n");
+
+        Console.Write("Numbers between 0 and 100, divisible by 3 : \n");
+        for (i = 1; i < 101; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write("{0}  ", i);
+                sum += i;
+            }
+        }
+
+    }
+}
+
---
+++ SumofNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
+        }
+    }
+}
